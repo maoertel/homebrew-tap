344dc590a84bad5d4122b8fcee74e268113e5ed2
Mathias Oertel <mathias.oertel@commercetools.com>
2021-10-30 22:46:52 +0200

    add support for arm64 macs

--- Formula/hoconvert.rb
+++ Formula/hoconvert.rb
@@ -7,6 +7,10 @@ class Hoconvert < Formula
     url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-darwin-amd64.tar.gz"
     sha256 "324afcaed854e57eddfd74204737ec4ffa793a2c336b2d418e352bb65a93c374"
   end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-darwin-arm64.tar.gz"
+    sha256 "fb39ecf4a08818faa9a00485e2195c3f29e7f10ecd71443bf80c82fa24dea4ad"
+  end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-linux-amd64.tar.gz"
     sha256 "7c4269fe513e3b6cdfb42fc64e79afb363283b3e4123ea241b11614fe39a5f38"
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Homebrew tap
 
-Tap to publish applications to `brew`.
+Tap to publish applications with `brew`. Tap it with:
+
+```bash
+brew tap maoertel/tap
+```
+
+## hoconvert
+
+- [hoconvert](https://github.com/maoertel/hoconvert): CLI tool to convert HOCON into JSON or YAML.
+- install for `macOS/amd64`, `macOS/arm64` or `linux/amd64` with:
+
+```bash
+brew install hoconvert
+```
