c1b3d5ea63898cff0f25e872c7d2dab3d0f45b8f
Mathias Oertel <mathias.oertel@commercetools.com>
2022-05-27 19:18:59 +0200

    Bump hoconvert version to v0.1.3

--- Formula/hoconvert.rb
+++ Formula/hoconvert.rb
@@ -1,20 +1,20 @@
 class Hoconvert < Formula
   desc "CLI tool to convert HOCON into valid JSON or YAML written in Rust."
-  version "0.1.2"
+  version "0.1.3"
   homepage "https://github.com/maoertel/hoconvert"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-darwin-amd64.tar.gz"
-    sha256 "324afcaed854e57eddfd74204737ec4ffa793a2c336b2d418e352bb65a93c374"
+    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.3/hoconvert-v0.1.3-darwin-amd64.tar.gz"
+    sha256 "5e76dfee6e19ecfe6b4429c26ad546b1cfc3c70cd9b3450cf12a8ece8239b4f9"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-darwin-arm64.tar.gz"
-    sha256 "fb39ecf4a08818faa9a00485e2195c3f29e7f10ecd71443bf80c82fa24dea4ad"
+    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.3/hoconvert-v0.1.3-darwin-arm64.tar.gz"
+    sha256 "8c2380dde65bea4d888958de28e8b2b51da9851545ed86a37c3fcf5f04a3f4d9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.2/hoconvert-v0.1.2-linux-amd64.tar.gz"
-    sha256 "7c4269fe513e3b6cdfb42fc64e79afb363283b3e4123ea241b11614fe39a5f38"
+    url "https://github.com/maoertel/hoconvert/releases/download/v0.1.3/hoconvert-v0.1.3-linux-amd64.tar.gz"
+    sha256 "6c36e4ffac3b4eb022c2b94b9703395e50e6d7dde064a3402aa13c20ea9f64d2"
   end
 
   def install
