aed04e0f40ead1ee9a878fd4c18b35834b5df645
Mathias Oertel <mathias.oertel@commercetools.com>
2023-09-07 23:47:42 +0200

    Increase version of ftdc to 0.1.4

--- Formula/ftdc.rb
+++ Formula/ftdc.rb
@@ -1,20 +1,20 @@
 class Ftdc < Formula
   desc "Command line tool to download FTDC data from mongodb clusters."
-  version "0.1.3"
+  version "0.1.4"
   homepage "https://github.com/maoertel/mongodb-ftdc"
   license "MIT"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.3/ftdc-v0.1.3-macOS-darwin-amd64.tar.gz"
-    sha256 "d0117ba190ed0afbc048d84ad3d8a883e960158d451ece0758f0f48a0593b0b5"
-  end
+  # if OS.mac? && Hardware::CPU.intel?
+  #   url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.4/ftdc-v0.1.4-macOS-darwin-amd64.tar.gz"
+  #   sha256 ""
+  # end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.3/ftdc-v0.1.3-macOS-darwin-arm64.tar.gz"
-    sha256 "209fd2d50f9a5c2faa490ac58772a9395f12f93e0e5f517f10e65e5bc2b4bb3d"
+    url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.4/ftdc-v0.1.4-macOS-darwin-arm64.tar.gz"
+    sha256 "fddef523c39df3fff52b1a4db215eebfa6624428629b9fb967d5c4e37922b900"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.3/ftdc-v0.1.3-linux-amd64.tar.gz"
-    sha256 "7b42b3cae56af5ef2660afeee89dc4e66693a6dddad63798b1af762167a90768"
+    url "https://github.com/maoertel/mongodb-ftdc/releases/download/v0.1.4/ftdc-v0.1.4-linux-amd64.tar.gz"
+    sha256 "a77f56ad6bb198feeace2e24bc3dc82f317778b718919d44e6fda53d8b660479"
   end
 
   def install
