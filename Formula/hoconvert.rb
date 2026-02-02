6fd17bd0f504f46141921911e3fef5f669de38c3
Mathias Oertel <maoertel@users.noreply.github.com>
2026-02-02 09:48:36 +0100

    Update hoconvert to v1.1.0 (#6)
    Co-authored-by: maoertel <12240147+maoertel@users.noreply.github.com>

--- Formula/hoconvert.rb
+++ Formula/hoconvert.rb
@@ -1,20 +1,24 @@
 class Hoconvert < Formula
-  desc "CLI tool to convert HOCON into valid JSON or YAML written in Rust."
-  version "1.0.0"
+  desc "CLI tool to convert HOCON into valid JSON, YAML, or TOML written in Rust."
+  version "1.1.0"
   homepage "https://github.com/maoertel/hoconvert"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/hoconvert/releases/download/v1.0.0/hoconvert-v1.0.0-darwin-amd64.tar.gz"
-    sha256 "eb88b23fb32df81fc804bea772f79dd04475b32677337bbb480843ed5b592a0b"
+    url "https://github.com/maoertel/hoconvert/releases/download/v1.1.0/hoconvert-v1.1.0-darwin-amd64.tar.gz"
+    sha256 "8aa6b6d19b81ce9fc9014c5aca48025246f6cfc6f093550a4affd0de54f5185e"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/maoertel/hoconvert/releases/download/v1.0.0/hoconvert-v1.0.0-darwin-arm64.tar.gz"
-    sha256 "1434d76f4aba725abaf81366dd47493e665de3fcabf4c24eaad8e9bc7b16a333"
+    url "https://github.com/maoertel/hoconvert/releases/download/v1.1.0/hoconvert-v1.1.0-darwin-arm64.tar.gz"
+    sha256 "0f00252c343222172e872b558495e6e07a90460539bea0f311d0297dd92c090e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/maoertel/hoconvert/releases/download/v1.0.0/hoconvert-v1.0.0-linux-amd64.tar.gz"
-    sha256 "de3012b85d2d172788676c2a83806a0332da29000bb88d6a205c0f16e9b68058"
+    url "https://github.com/maoertel/hoconvert/releases/download/v1.1.0/hoconvert-v1.1.0-linux-amd64.tar.gz"
+    sha256 "e5679bad6312cdb1cdd761599fb832e1e798db0bb328a0ada2f7b6e20c6a0cff"
+  end
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/maoertel/hoconvert/releases/download/v1.1.0/hoconvert-v1.1.0-linux-arm64.tar.gz"
+    sha256 "f5c57f8295d799264ddde21163a4aac17b5f68454625066680d54b8e1dc5e553"
   end
 
   def install
